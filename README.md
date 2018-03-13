b422f7203cc4abf5b1395a9a8fbfeab21fcce2c0
darmawan01 <darmawan.zulkifli@gmail.com>
2018-03-13 16:50:34 +0800

    added auth routes

---
+++ README.md
@@ -0,0 +1,8 @@
+# Compile Backend Web Apps
+# Run On Local
+    - rename .env.example to .env then set database name,user,password
+    - php artisan migrate
+    - php artisan passport:install
+# Api Routes
+
+    - php artisan route:list
---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Laravel\Passport\Passport;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'App\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        // register the routes necessary to issue access tokens and revoke access tokens, clients, and personal access tokens
+        Passport::routes();
+    }
+}
